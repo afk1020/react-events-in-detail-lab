1231a60944f3edc6b1df97fe950dc17850c2ab74
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-29 11:43:58 -0500

    commit tree a702d3283ca4f32616d70e57160ad3bc5b61ede0

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component{
+   
+
+
+    setTimeout () => {
+    this.props.onDelayedClick(this.props.delay)
+    }
+
+    render(){
+        return (
+        <button onClick={this.setTimeout}> Delayed </button>
+        )
+    }
+
+
+export default DelayedButton;
